5d7e9a27cd372d4d0a4654ce4d92f7ded04ff165
Tai Phan Van <phanvantai.sh@gmail.com>
2021-07-28 14:38:51 +0700

    Initial Commit

---
+++ Shared/NovflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovflixApp.swift
+//  Shared
+//
+//  Created by Tai Phan Van on 28/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NovflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
