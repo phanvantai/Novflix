dd9cadaea5a53a573ee041de00bb162d73a05f3f
Tai Phan Van <phanvantai.sh@gmail.com>
2021-07-30 21:22:33 +0700

    Update top row selection

---
+++ iOS/Home/HomeTopRow.swift
@@ -0,0 +1,8 @@
+//
+//  HomeTopRow.swift
+//  Novflix
+//
+//  Created by Tai Phan Van on 30/07/2021.
+//
+
+import Foundation
---
+++ iOS/Home/TopRowButton.swift
@@ -0,0 +1,20 @@
+//
+//  TopRowButton.swift
+//  Novflix
+//
+//  Created by Tai Phan Van on 30/07/2021.
+//
+
+import SwiftUI
+
+struct TopRowButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopRowButton_Previews: PreviewProvider {
+    static var previews: some View {
+        TopRowButton()
+    }
+}
